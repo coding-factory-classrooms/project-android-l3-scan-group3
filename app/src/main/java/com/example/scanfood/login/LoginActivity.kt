e6cd3cb5bd67fff44535489fb06f37c1f494764d
Gustin <florian.gustin.business@gmail.com>
2021-03-10 11:16:20 +0100

    feat : archi

---
+++ app/src/main/java/com/example/scanfood/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.scanfood.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.scanfood.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
