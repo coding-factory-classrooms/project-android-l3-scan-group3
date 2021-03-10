8b3151655fa0d9852f0ce091fd8340ce909f07cd
Riyad <riyad.achour@edu.itesica.fr>
2021-03-10 16:37:10 +0100

    fix : login page

--- app/src/main/java/com/example/scanfood/login/LoginActivity.kt
+++ app/src/main/java/com/example/scanfood/login/LoginActivity.kt
@@ -3,37 +3,79 @@ package com.example.scanfood.login
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
+import androidx.core.widget.doAfterTextChanged
+import androidx.lifecycle.Observer
 import com.example.scanfood.R
 import com.example.scanfood.databinding.ActivityLoginBinding
+import androidx.lifecycle.ViewModel
+import androidx.activity.viewModels
+
+
+private const val TAG = "LoginActivity"
 
 class LoginActivity : AppCompatActivity() {
 
-    private lateinit var binding : ActivityLoginBinding
+    private val model: LoginViewModel by viewModels()
+
+    private lateinit var binding: ActivityLoginBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        model.getState().observe(this, Observer { updateUI(it!!) })
+
+        model.updateLogin(
+            binding.usernameEditText.text.toString(),
+            binding.passwordEditText.text.toString()
+        )
 
 
-        binding.loginButton.setOnClickListener{
-            if(validLogin(binding.usernameEditText.text.toString(), binding.passwordEditText.text.toString())) {
-                Toast.makeText(this@LoginActivity,
-                    " Login OK ! Navigation vers la liste de films",
-                    Toast.LENGTH_SHORT).show()
-              //  val intent  = Intent(this, nomDeLaClasse)
-              //  startActivity(intent)
-            }else {
-                Toast.makeText(this@LoginActivity,
-                    " Utilisateur / mot de passe incorrect",
-                    Toast.LENGTH_LONG).show()
+        binding.loginButton.setOnClickListener {
+            model.login(binding.usernameEditText.text.toString(), binding.passwordEditText.text.toString())
+        }
+        binding.usernameEditText.doAfterTextChanged { text ->
+            model.updateLogin(
+                binding.usernameEditText.text.toString(),
+                binding.passwordEditText.text.toString()
+            )
+        }
+        binding.passwordEditText.doAfterTextChanged { text ->
+            model.updateLogin(
+                binding.usernameEditText.text.toString(),
+                binding.passwordEditText.text.toString()
+            )
+        }
+    }
+
+    private fun updateUI(state: LoginViewModelState) {
+        Log.i(TAG, "updateUI : state=$state")
+        when (state) {
+            is LoginViewModelState.Failure -> {
+                binding.loginButton.isEnabled = state.loginButtonEnabled
+                Toast.makeText(this@LoginActivity, "Login NOK", Toast.LENGTH_SHORT).show()
+            }
+            is LoginViewModelState.Loading -> {
+                binding.loginButton.isEnabled = state.loginButtonEnabled
+                Toast.makeText(this@LoginActivity, "Loading..", Toast.LENGTH_SHORT).show()
+            }
+            LoginViewModelState.Success -> {
+                binding.loginButton.isEnabled = state.loginButtonEnabled
+                Toast.makeText(this@LoginActivity, "Login OK", Toast.LENGTH_SHORT).show()
+                navigateToMovieList()
+            }
+            is LoginViewModelState.UpdateLogin -> {
+                binding.loginButton.isEnabled = state.loginButtonEnabled
             }
         }
     }
 
-    private fun validLogin(username: String, password: String): Boolean =
-        username == "kotlin" && password == "rocks"
-
+    private fun navigateToMovieList() {
+      /*  val intent = Intent(this, MovieListActivity::class.java)
+        startActivity(intent)
+        finish()*/
+    }
 }
---
+++ app/src/main/java/com/example/scanfood/login/LoginViewModel.kt
@@ -0,0 +1,38 @@
+package com.example.scanfood.login
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+sealed class LoginViewModelState(
+        open val errorMessage: String = "",
+        open val loginButtonEnabled: Boolean = false
+    ) {
+        object Loading : LoginViewModelState()
+        object Success : LoginViewModelState()
+        data class Failure(override val errorMessage: String) :
+            LoginViewModelState(errorMessage = errorMessage, loginButtonEnabled = true)
+
+        data class UpdateLogin(override val loginButtonEnabled: Boolean) :
+            LoginViewModelState(loginButtonEnabled = loginButtonEnabled)
+    }
+
+    class LoginViewModel : ViewModel() {
+        private val state = MutableLiveData<LoginViewModelState>()
+        fun getState(): LiveData<LoginViewModelState> = state
+
+        fun login(username: String, password: String) {
+            val isValid = validLogin(username, password)
+            if (isValid) state.value = LoginViewModelState.Success else state.value =
+                LoginViewModelState.Failure("Invalid credentials")
+        }
+
+        private fun validLogin(username: String, password: String): Boolean = username == "kotlin" && password == "rocks"
+        fun updateLogin(username: String, password: String) {
+            val buttonEnabled = username.isNotBlank() && password.isNotBlank()
+            state.value = LoginViewModelState.UpdateLogin(loginButtonEnabled = buttonEnabled)
+        }
+
+    }
+
+    fun validLogin(username: String, password: String): Boolean = username == "kotlin" && password == "rocks"
