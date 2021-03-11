15fad16dbcf12f264c976cbb9ad365cb0dad4458
Gustin <florian.gustin.business@gmail.com>
2021-03-11 18:26:16 +0100

    feat : list item switch working

--- app/src/main/java/com/example/scanfood/application/history/HistoryListViewModel.kt
+++ app/src/main/java/com/example/scanfood/application/history/HistoryListViewModel.kt
@@ -1,11 +1,14 @@
 package com.example.scanfood.application.history
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.scanfood.domain.Product
+import com.example.scanfood.domain.toColorCategory
 import java.util.*
 
+const val TAG = "HistoryListViewModel"
 
 sealed class HistoryListViewModelState(
     open val errorMessage: String = "",
@@ -17,23 +20,34 @@ sealed class HistoryListViewModelState(
     data class CameraOff(override val cameraEnabled: Boolean) : HistoryListViewModelState(cameraEnabled = cameraEnabled)
     data class Failure(override val errorMessage: String) :
         HistoryListViewModelState(errorMessage = errorMessage)
-    data class Success(override val products: List<Product>) : HistoryListViewModelState()
+    data class Changed(override val products: List<Product>) : HistoryListViewModelState()
 }
 
 
-
 class HistoryListViewModel : ViewModel() {
-
-
+    private var placeholderProduct: Product =
+        Product(
+            0,
+            "Tarte aux pommes",
+            "https://static.750g.com/images/600-600/9823eb627203c878f3e36d72f8ce6d1c/tarte-aux-pommes.jpg",
+            Date(),
+            Date()
+        )
+    private var products = mutableListOf<Product>()
     private val state = MutableLiveData<HistoryListViewModelState>()
     fun getState(): LiveData<HistoryListViewModelState> = state
+    init { state.value = HistoryListViewModelState.Empty }
 
-    fun routerCamera(){
-        //TODO : implements
-    }
 
-    fun scanQr(){
+    fun scan(){
         //TODO : implements
+        if(!state.value!!.cameraEnabled){
+            usePlaceHolderData()
+            Log.d(TAG, "simulate data wihout camera")
+        }
+        Log.i(TAG, "scanning...")
+
+        // do something
     }
 
     fun getQrData(){
@@ -41,26 +55,15 @@ class HistoryListViewModel : ViewModel() {
     }
 
     fun toggleCamera(){
-        state.postValue(
-            HistoryListViewModelState.CameraOff(
-                !state.value!!.cameraEnabled
-            )
-        )
+        state.postValue(HistoryListViewModelState.CameraOff(cameraEnabled = !state.value!!.cameraEnabled))
     }
 
-    fun usePlaceHolderData(){
-        val product: Product =
-            Product(
-                0,
-                "Tarte aux pommes",
-                "https://static.750g.com/images/600-600/9823eb627203c878f3e36d72f8ce6d1c/tarte-aux-pommes.jpg",
-                Date(),
-                Date()
-            )
-        addItem(product)
+    private fun usePlaceHolderData(){
+        if(!products.contains(placeholderProduct)) addItem(placeholderProduct)
     }
 
     fun clearData(){
+        products = mutableListOf()
         state.postValue(HistoryListViewModelState.Empty)
     }
 
@@ -74,46 +77,35 @@ class HistoryListViewModel : ViewModel() {
     }
 
     fun addItem(product: Product){
-        val adding: List<Product> = state.value!!.products.plus(product)
-        state.postValue(
-            HistoryListViewModelState.Success(
-                products = adding
-            )
-        )
+        products.add(product)
+        state.postValue(HistoryListViewModelState.Changed(products = products))
+        Log.i(TAG, "product added")
     }
 
-//    fun updateItem(id: Int){
-//        val next: List<Product> = state.value!!.products.map { item -> {
-//            if (item.id == id) item.scanDate = Date() else item
-//        } } as List<Product>
-//        state.postValue(HistoryListViewModelState.Success(products = next))
-//    }
-
-    fun deleteItem(product: Product){
-        val next: List<Product> = state.value!!.products.minus(product)
-        state.postValue(
-            HistoryListViewModelState.Success(
-                products = next
-            )
-        )
+    fun updateItem(index: Int, product: Product){
+        product.scanDate = Date()
+        products[index] = product
+        state.postValue(HistoryListViewModelState.Changed(products = products))
+        Log.i(TAG, "product updated")
     }
+
+    fun deleteItem(index: Int, product: Product){
+        products.remove(product)
+        state.postValue(HistoryListViewModelState.Changed(products = products))
+        Log.w(TAG, "product deleted")
+    }
+
     fun orderByDate(){
-        val next: List<Product> = state.value!!.products.sortedBy { it.dateExp }
-        state.postValue(
-            HistoryListViewModelState.Success(
-                products = next
-            )
-        )
+        products.sortedBy { it.dateExp }
+        state.postValue(HistoryListViewModelState.Changed(products = products))
+        Log.i(TAG, "products now ordered by expiration date")
     }
 
-//    fun filterByColorDuration(color: Int){
-//        val next: List<Product> = state.value!!.products.map {
-//            if(it.toColorCategory() != color ) it.hide = true else it.hide
-//        }  as List<Product>
-//        state.postValue(HistoryListViewModelState.Success(products = next))
-//    }
-
-    fun onFABLongPressed(){
-        toggleCamera()
+    fun filterByColorDuration(color: Int){
+        products.map { if(it.toColorCategory() != color ) it.hide = true else it.hide }
+        state.postValue(HistoryListViewModelState.Changed(products = products))
+        Log.i(TAG, "products now filtered by duration color")
     }
+
 }
+
